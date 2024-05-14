5d194634d2f9db65f4a3c2cf8243dc7addd07579
Rodrigo Borquez <rborquezdiaz@gmail.com>
2024-05-13 23:32:36 -0600

    Initial Commit + Bazel Workspace

---
+++ SwiftLintPOC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftLintPOC
+//
+//  Created by Rodrigo Borquez Diaz on 13/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
